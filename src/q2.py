3c8de20ba5c5a5bc572f6321c83f2a8e485b1eac
stuffkaicodes <e0202418@u.nus.edu>
2025-08-04 16:38:40 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -1,4 +1,11 @@
 def find_and_replace(lst, find_val, replace_val):
+    lst_copy = lst
+    if isinstance(lst,list): 
+        for i in lst_copy:
+            if i == find_val:
+                ind = lst_copy.index(i)
+                lst[ind] = replace_val
+    return lst
     """
     Task 1
     - Create a function that searches for all occurrences of a value (find_val) in a given list (lst) and replaces them with another value (replace_val).
