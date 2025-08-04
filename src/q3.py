c478dccbfd26e267073ce848997f4f775e248bfb
stuffkaicodes <e0202418@u.nus.edu>
2025-08-04 16:51:58 +0800

    Update q3.py

--- src/q3.py
+++ src/q3.py
@@ -1,11 +1,15 @@
 def update_dictionary(dct, key, value):
+    if key in dct: )
+        dct[key] = value
+        return dct 
+    else:
+        return 
     """
     Task 1
     - Create a function that updates a dictionary (dct) with a new key-value pair.
     - If the key already exists in dct, print the original value, then update its value.
     - Return the updated dictionary.
     """
-    return
 
 
 # Task 2
